cc56b8aef9379143cbacb64e76dea9cfb2a87e33
francescotermite <francesco.termite6@gmail.com>
2025-01-09 08:31:34 +0000

    ultima versione

--- app.py
+++ app.py
@@ -1,12 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
+app.config["MYSQL_HOST"]="138.41.20.102"
+app.config["MYSQL_PORT"]= 53306
+app.config["MYSQL_DB"]= "w3schools"
+app.config["MYSQL_USER"]="ospite"
+app.config["MYSQL_PASSWORD"]="ospite"
 
-lista=(("Capocollo", "SC1", 2), ("Marmellata", "SC2", 3), ("Caffè", "SC3", 1.50))
-
+mysql = MySQL(app)
 @app.route("/")
-def homepage():
-    return render_template("Homepage.html")
+def home():
+    return render_template("Homepage.html", titolo="HomePage")
 
-@app.route("/Product")
-app.run(debug=True)
+@app.route('/products')
+def products():
+    cursor = mysql.connection.cursor()
+    cursor.execute("SELECT * FROM products")
+    products = cursor.fetchall()
+    cursor.close()
+    return render_template('Products.html', title="Prodotti", products=products)
+
+@app.route('/category')
+def category():
+    return render_template("Category.html")
+
+
+
+
+  
